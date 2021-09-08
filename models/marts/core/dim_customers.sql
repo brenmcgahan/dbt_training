cb20001f7ced6e76c9ab751dbb475e042d9e167e
brenmcgahan <89041521+brenmcgahan@users.noreply.github.com>
2021-09-08 14:30:11 -0500

    refactored based on examples in training

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,45 +1,20 @@
 with customers as (
-
     select * from {{ ref('stg_customers')}}
-
 ),
-
 orders as (
-
-    select * from {{ ref('stg_orders') }}
-
+    select * from {{ ref('fct_orders')}}
 ),
-
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-
-    group by 1
-
-),
-
-order_payments as (
-
-    select 
-        orders.customer_id,
-        sum(payments.amount) lifetime_value
-
-    from orders
-
-    left join {{ ref('stg_payments') }} as payments using (order_id)
-
     group by 1
 ),
-
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
@@ -47,13 +22,8 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        coalesce(order_payments.lifetime_value, 0) as lifetime_value
-
+        customer_orders.lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
-
-    left join order_payments using (customer_id)
 )
-
 select * from final
