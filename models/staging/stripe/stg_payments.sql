fe959c2a5dcbbe989f8391aa6dd361cfb28b47cc
brenmcgahan <89041521+brenmcgahan@users.noreply.github.com>
2021-09-09 13:23:24 -0500

    added source to models/stripe

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,8 +1,9 @@
 select
     id as payment_id,
     orderid as order_id,
-    paymentmethod,
+    paymentmethod as payment_method,
     status,
-    amount / 100 as amount
+    amount / 100 as amount,
+    created as created_at
 
-from training.stripe.payment
+from {{ source('stripe', 'payment') }}
