4716a231b65b2662570288fc8ad02085be499a43
brenmcgahan <89041521+brenmcgahan@users.noreply.github.com>
2021-09-10 12:24:25 -0500

    added seed data

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,9 +1,15 @@
 with customers as (
     select * from {{ ref('stg_customers')}}
 ),
+
 orders as (
     select * from {{ ref('fct_orders')}}
 ),
+
+employees as (
+    select * from {{ ref('employees')}}
+),
+
 customer_orders as (
     select
         customer_id,
@@ -14,16 +20,20 @@ customer_orders as (
     from orders
     group by 1
 ),
+
 final as (
     select
         customers.customer_id,
         customers.first_name,
         customers.last_name,
+        employees.employee_id is not null as is_employee,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
         customer_orders.lifetime_value
     from customers
     left join customer_orders using (customer_id)
+    left join employees using (customer_id)
 )
+
 select * from final
